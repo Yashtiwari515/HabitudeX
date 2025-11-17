8e19e13156f5d34c10e1718f4c97053ee2663f30
Yash Tiwari <tiwariyash515@gmail.com>
2025-11-17 22:36:15 +0530

    Deploying

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-// import cors from "cors";
-// import morgan from "morgan";
-// import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
 
 import userRoutes from "./routes/userRoutes.js";
 import journalRoutes from "./routes/journalRoutes.js";
@@ -17,20 +17,20 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin: "*",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
-// app.use(helmet());
-// app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(helmet());
+app.use(morgan("dev"));
 
-// app.use((req, res, next) => {
-//   res.set("Cache-Control", "no-store");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.set("Cache-Control", "no-store");
+  next();
+});
 
 app.use("/api/user", userRoutes);
 app.use("/api/journal", journalRoutes);
